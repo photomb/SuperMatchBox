5244227287f4ec0c02c31638b2bfbd1812098895
photomb <photomb09@gmail.com>
2025-02-21 16:31:56 +0100

    data load bugs fix

--- js/progress.js
+++ js/progress.js
@@ -16,36 +16,22 @@ document.addEventListener("DOMContentLoaded", () => {
         '../media/twinkle.gif'
     ]
 
-    const preloadSounds = [
-        '../media/blip.mp3',
-        '../media/close.mp3',
-        '../media/correct.mp3',
-        '../media/menu-selection.mp3',
-        '../media/mouse-special.mp3',
-        '../media/mouse-start.mp3',
-        '../media/short-success.mp3',
-        '../media/special.mp3',
-    ]
-
     let progress = 0
     let videoLoaded = false
     let audioLoaded = false
     let fontsLoaded = false
     let imagesLoadedCount = 0
-    let soundsLoadedCount = 0
 
     sessionStorage.clear()
 
     function updateProgress() {
         progress = 0
         
-        if (videoLoaded) progress += 30
-        if (audioLoaded) progress += 30
+        if (videoLoaded) progress += 50
+        if (audioLoaded) progress += 25
         if (fontsLoaded) progress += 10
         if (imagesLoadedCount === preloadImages.length) progress += 15
-        if (soundsLoadedCount === preloadSounds.length) progress += 15
 
-        console.log(`progress : ${progress}`)
         progressFill.style.width = `${progress}%`
         loadingText.textContent = `Loading ... ${progress}%`
 
@@ -85,24 +71,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 sessionStorage.setItem("imagesLoadedCount", imagesLoadedCount)
                 updateProgress()
             }
+            console.log("Images ok")
         }
     })
-
-    //Are sounds ready ?
-    preloadSounds.forEach(sound => {
-        if(sessionStorage.getItem(sound)) {
-            soundsLoadedCount++
-            updateProgress()
-        } else {
-            const audio = new Audio(sound)
-            audio.oncanplay = () => {
-                soundsLoadedCount++
-                sessionStorage.setItem(sound, "true") //Onload sound ok
-                sessionStorage.setItem("soundsLoadedCount", soundsLoadedCount)
-                updateProgress()
-            }
-        }
-    }) 
 
     //Is video ready ?
     const onVideoload = () => {
@@ -115,6 +86,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (videoBG.readyState >= 3) onVideoload() //3 = HAVE_FUTURE_DATA for readyState
     videoBG.addEventListener("canplay", () => { videoLoaded = true })
     videoBG.addEventListener("canplaythrough", () => { videoLoaded = true })
+    console.log("Video ok")
 
     //Is audio ready ?
     const onAudioLoad = () => {
@@ -127,6 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (audioIndex.readyState >= 3) onAudioLoad()
         audioIndex.addEventListener("canplay", () => { audioLoaded = true })
         audioIndex.addEventListener("canplaythrough", () => { audioLoaded = true })
+        console.log("Audio ok")
 
     // Is fonts ready ?
     if(!fontsLoaded) {
@@ -140,6 +113,7 @@ document.addEventListener("DOMContentLoaded", () => {
             fontsLoaded = true
             sessionStorage.setItem("fontsLoaded", "true")
             updateProgress()
+            console.log("Fonts ok")
         })
         .catch((zut) => {
             console.error("Error loading fonts:", zut)
