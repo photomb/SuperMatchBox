afee49e487ab9dc57d592f69bb5e8ee2c0510a33
Bertrand MARIE <photomb09@gmail.com>
2025-03-27 15:42:21 +0100

    modification for smartphone good use

--- js/progress.js
+++ js/progress.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const progressFill = document.querySelector(".progress-fill")
     const loadingText = document.getElementById("loading-text")
     const sendGameButton = document.getElementById("sendGameButton")
-    
+
     const preloadImages = [
         'img/close.png',
         'img/joker.png',
@@ -85,6 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
     videoBG.addEventListener("canplaythrough", () => {
         if (!videoLoaded) {
             videoLoaded = true
+            localStorage.setItem("videoLoaded", "true")
             console.log("VIDEO OK")
             updateProgress()
         }
