0252b9b6c96a72f6a684f73aa5611d77db451177
Bertrand MARIE <photomb09@gmail.com>
2025-03-27 11:01:25 +0100

    restore progress.js file on main

--- js/progress.js
+++ js/progress.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const loadingText = document.getElementById("loading-text")
     const sendGameButton = document.getElementById("sendGameButton")
     
-    
     const preloadImages = [
         '../img/close.png',
         '../img/joker.png',
@@ -27,8 +26,6 @@ document.addEventListener("DOMContentLoaded", () => {
     function updateProgress() {
         progress = 0
         
-        if (videoLoaded) progress += 40
-        if (audioLoaded) progress += 30
         if (videoLoaded) progress += 40
         if (audioLoaded) progress += 30
         if (fontsLoaded) progress += 10
@@ -102,8 +99,6 @@ document.addEventListener("DOMContentLoaded", () => {
             audioLoaded = true
             localStorage.setItem("audioLoaded", "true")
             console.log("AUDIO OK")
-            localStorage.setItem("audioLoaded", "true")
-            console.log("AUDIO OK")
             updateProgress()
         }
     }, { once: true })
@@ -120,12 +115,10 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(() => {
             fontsLoaded = true
             localStorage.setItem("fontsLoaded", "true")
-            localStorage.setItem("fontsLoaded", "true")
             updateProgress()
             console.log("FONTS OK")
         })
         .catch((zut) => {
-            console.error("Error loading fonts : ", zut)
             console.error("Error loading fonts : ", zut)
         })
     }
