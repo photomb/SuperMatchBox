380f2681fb163f8f2292f6b663fd8b3ea2ea2de9
Bertrand MARIE <photomb09@gmail.com>
2025-06-27 00:22:49 +0200

    random ok

--- js/random.js
+++ js/random.js
@@ -83,7 +83,7 @@ export function specialBtn() {
 
             setTimeout(() => {
                 gifEffect.remove()
-            }, 20000) // remove after 2 seconds
+            }, 2000) // remove after 2 seconds
         })
     } else {
         document.querySelectorAll('.special').forEach(button => { 
